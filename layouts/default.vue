976ce986609ebedd24e4bfa0615f4b313539ad6d
backendlab-app <info@python-private-package-index.com>
2018-11-08 21:29:21 +0000

    adding a header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
   <v-app>
+    <v-toolbar dark height="95px" color="cyan" app>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>
+        <div>
+          <div class="display-1 text-xs-center">Microblog</div>
+          <div class="title text-xs-center">Built with BackendLab and VueJS</div>
+        </div>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
     <system-alert></system-alert>
     <v-content>
       <nuxt/>
